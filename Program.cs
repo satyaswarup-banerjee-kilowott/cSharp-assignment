34f027df2b910723ca98f420a59aabb403b62360
satyaswarup-banerjee-kilowott <92801510+satyaswarup-banerjee-kilowott@users.noreply.github.com>
2021-11-29 12:46:24 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+Console.WriteLine("Hello, World! \n");
+
+//Example of different ways of including print statements
+
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
