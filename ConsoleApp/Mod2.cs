c1bc2bf94c202f7adc62a26b360d89df766c1bff
satyaswarup-banerjee-kilowott <92801510+satyaswarup-banerjee-kilowott@users.noreply.github.com>
2021-11-30 14:02:17 +0530

    Initial commit using top-level statements

---
+++ ConsoleApp/Mod2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Mod2
+    {
+        public static void ShowOutput()
+
+        {
+
+            string name = "Bob";
+            int messages = 3;
+            decimal temperature = 34.4m;
+
+            Console.Write("Hello, ");
+            Console.Write(name);
+            Console.Write("! You have ");
+            Console.Write(messages);
+            Console.Write(" in your inbox. The temperature is ");
+            Console.Write(temperature);
+            Console.Write(" celsius.");
+
+        }
+    }
+}
