2a656fcbcff536daadfee668c88d3cb9cd5e211c
satyaswarup-banerjee-kilowott <92801510+satyaswarup-banerjee-kilowott@users.noreply.github.com>
2021-12-01 14:15:58 +0530

    Initial Commit

---
+++ ConsoleApp/Mod5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Mod5
+    {
+        public static void ShowOutput()
+        {
+
+            int firstValue = 500;
+            int secondValue = 200;
+            int largerValue;
+            largerValue = Math.Max(firstValue, secondValue);
+            Console.WriteLine(largerValue);
+        }
+    }
+}
