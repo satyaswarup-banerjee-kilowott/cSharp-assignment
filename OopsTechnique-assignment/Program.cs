174fb26f06b294aaca541d679781e3924f56baf9
satyaswarup-banerjee-kilowott <92801510+satyaswarup-banerjee-kilowott@users.noreply.github.com>
2021-12-01 14:19:05 +0530

    Initial commit

---
+++ OopsTechnique-assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OopsTechnique_assignment
+{
+    internal class Program
+    {
+        static int[] data = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19 };
+        static void Main(string[] args)
+        {
+            var spObject = new Stats();
+            var spObject1 = new Stats();
+            var spObject2 = new Stats();
+
+            spObject.Numbers = data;
+            spObject1.Numbers = data;
+            spObject2.Numbers = data;
+
+            var mean = spObject.CalculateMean();
+            var median = spObject1.CalculateMedian();
+            var mode = spObject2.CalculateMode();
+
+            Console.WriteLine($"Mean: {mean}");
+            Console.WriteLine($"Median: {median}");
+            Console.WriteLine($"Mode: {mode}");
+
+
+        }
+    }
+}
+
---
+++ OopsTechnique-assignment/Stats.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsTechnique_assignment
+{
+    internal class Stats
+    {
+        internal int[] Numbers { private get; set; }
+
+
+        internal double CalculateMean()
+        {
+            double m = 0;
+            double sum = 0;
+            int i;
+            int n = Numbers.Count();
+
+            for (i = 0; i < Numbers.Count(); i++)
+            {
+                sum += Numbers[i];
+            }
+            m = sum / Numbers.Count();
+
+
+            return m;
+        }
+
+        internal int CalculateMedian()
+        {
+            Array.Sort(Numbers);
+            int med = 0;
+            int n = Numbers.Count();
+            if (n % 2 != 0)
+            {
+                med = Numbers[n / 2];    
+
+            }
+            return med; 
+        }
+
+        internal int CalculateMode()
+        {
+            int i;
+            int mCount = 0;
+            int maxNum = 0;
+            for (i = 0; i < Numbers.Count(); i++)
+            {
+                int count = 0;
+
+                for (int j = 1; j < Numbers.Count() - 1; j++)
+                {
+                    if (Numbers[i] == Numbers[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count > mCount)
+                {
+                    mCount = count;
+                    maxNum = Numbers[i];
+
+                }
+            }
+            return maxNum;
+        }
+    }
+}
